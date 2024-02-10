88d24b9fbeb0b5443a45143215112022774eb6e1
Parichay Didwania <parichaydidwania@gmail.com>
2024-02-09 22:55:11 -0500

    config for vpc

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "project_id" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
+
+variable "vpc_name" {
+    type = string
+}
+
+variable "webapp_subnet_name" {
+    type = string
+}
+
+variable "webapp_subnet_cidr" {
+    type = string
+}
+
+variable "db_subnet_name" {
+    type = string
+}
+
+variable "db_subnet_cidr" {
+    type = string
+}
+
+variable "webapp_default_route_name" {
+    type = string
+}
---
+++ vpc.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = var.project_id
+  region = var.region
+}
+
+resource "google_compute_network" "vpc" {
+  name = var.vpc_name
+  routing_mode = "REGIONAL"
+  auto_create_subnetworks = false
+  delete_default_routes_on_create = true
+}
+
+resource "google_compute_subnetwork" "webapp-subnet" {
+  name          = var.webapp_subnet_name
+  ip_cidr_range = var.webapp_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_compute_subnetwork" "db-subnet" {
+  name          = var.db_subnet_name
+  ip_cidr_range = var.db_subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_compute_route" "webapp-default" {
+  name        = var.webapp_default_route_name
+  dest_range  = "0.0.0.0/0"
+  network     = google_compute_subnetwork.webapp-subnet.network
+  next_hop_gateway = "default-internet-gateway"
+}
