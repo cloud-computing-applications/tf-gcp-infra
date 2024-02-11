c755f0b73f26a06acbd49d5bed622a016dee21bd
Parichay Didwania <parichaydidwania@gmail.com>
2024-02-11 17:18:40 -0500

    added seperate region vars for each subnet

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "project_id" {
   type = string
 }
 
-variable "region" {
-  type = string
-}
-
 variable "vpc_name" {
   type = string
 }
@@ -18,11 +14,19 @@ variable "webapp_subnet_cidr" {
   type = string
 }
 
+variable "webapp_subnet_region" {
+  type = string
+}
+
 variable "db_subnet_name" {
   type = string
 }
 
 variable "db_subnet_cidr" {
+  type = string
+}
+
+variable "db_subnet_region" {
   type = string
 }
 
